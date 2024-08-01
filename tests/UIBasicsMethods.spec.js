bca111a3fcd9f8f08cbd85e5d2a59da57d467edb
nidhi <g.nidhi2015@gmail.com>
2024-08-01 23:38:42 +0100

    August1 commit

--- tests/UIBasicsMethods.spec.js
+++ tests/UIBasicsMethods.spec.js
@@ -1,5 +1,12 @@
 const{test, expect} = require('@playwright/test');
 test('Browser Playwright Test', async({page})=>{
-  await page.goto("https://rahulshettyacademy.com/client/auth/login");
-  
+  await page.goto("https://rahulshettyacademy.com/client");
+ await page.click(".btn1");
+ await page.locator("#firstName").fill("abc1");
+  await page.locator("#lastName").fill("xyz1");
+  await page.locator("#userEmail").fill("abc1@xyz1");
+  await page.locator("#userMobile").fill("1234567890");
+  await page.locator("#userPassword").fill("abc1@xyz1");
+  await page.locator("#confirmPassword").fill("abc1@xyz1");
+  await page.locator("#login").click();
 });
